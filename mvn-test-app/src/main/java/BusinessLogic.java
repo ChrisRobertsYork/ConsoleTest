963d484b51f9113b98e43707bc95d984597e6d71
chris.robertsyork <chris.robertsyork@excelian.com>
2013-11-15 11:09:02 +0000

    Submitting Java Project

---
+++ mvn-test-app/src/main/java/BusinessLogic.java
@@ -0,0 +1,17 @@
+
+public class BusinessLogic {
+	static int Add(int first, int second)
+	{
+		return first + second;
+	}
+	
+	static int Multiply(int first, int second)
+	{
+		return first * second;
+	}
+	
+	static int Subtract(int first, int second)
+	{
+		return first + second;
+	}
+}
